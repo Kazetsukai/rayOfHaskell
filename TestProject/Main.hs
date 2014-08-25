8d718cda9a920b6e45478407704a5fe09ec0facc
Kazetsukai <moeruze.burning+github@gmail.com>
2014-08-25 15:58:11 +1200

    Added rays

--- TestProject/Main.hs
+++ TestProject/Main.hs
@@ -1,12 +1,45 @@
 module Main where
 
 import System.Environment
-import Graphics.Gloss
 import Graphics.Gloss.Raster.Field
+import Data.Vector.Unboxed
+
+width = 1024
+height = 1024
+
+aspectRatio = (fromIntegral width :: Float) / (fromIntegral height :: Float)
 
 main :: IO ()
-main = 
+main = do
     animateField
-        (InWindow "Test" (600, 400) (10, 10))
+        (InWindow "Test" (width, height) (10, 10))
         (1,1)
-        (\t (x,y) -> rawColor x 0.3 t 1.0)
+        pixelColor
+
+pixelColor :: Float -> Point -> Color
+pixelColor t p = 
+	let Ray point vec = pixelRay p
+	    (x, y, z)     = vec
+	in rawColor x y z 1.0
+
+pixelRay :: Point -> Ray
+pixelRay (x, y) = Ray (0, 0, 0) (normalise (x, y, -1))
+
+normalise :: Vec3 -> Vec3
+normalise (x, y, z) = 
+	let len = lengthV (x, y, z)
+	in (x / len, y / len, z / len)
+
+lengthV :: Vec3 -> Float
+lengthV (x, y, z) = sqrt (x*x + y*y + z*z)
+
+addV :: Vec3 -> Vec3 -> Vec3
+addV (x1, y1, z1) (x2, y2, z2) = (x1+x2, y1+y2, z1+z2)
+subV :: Vec3 -> Vec3 -> Vec3
+subV (x1, y1, z1) (x2, y2, z2) = (x1-x2, y1-y2, z1-z2)
+multV :: Vec3 -> Float -> Vec3
+multV (x, y, z) a = (x*a, y*a, z*a)
+
+type Vec3 = (Float, Float, Float)
+type Point3 = Vec3
+data Ray = Ray Point3 Vec3
